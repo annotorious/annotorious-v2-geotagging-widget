4a00d8b074b30a6e81584b2411a83be27cf8e929
Rainer Simon <aboutgeo@gmail.com>
2022-05-13 14:17:27 +0200

    React-Leaflet boilerplate code

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,26 @@
+import React, { useRef } from 'react';
+import { Map, TileLayer } from 'react-leaflet';
+
+import './index.css';
+import 'leaflet/dist/leaflet.css';
+
 const GeoTaggingPlugin = config => props => {
 
+  const mapRef = useRef();
+
   return (
-    <div>Hello World</div> 
+    <div className="r6o-geotagging r6o-widget">
+      <Map 
+        ref={mapRef}
+        zoom={7}
+        preferCanvas={true}
+        attributionControl={false}
+        center={[48, 16]}
+        style={{ height:'100%' }}>
+        <TileLayer
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}" />
+      </Map>  
+    </div>
   )
 
 }
