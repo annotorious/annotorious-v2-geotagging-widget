4a0f6f460f529d4c9f4e4158d20035703aaf7591
Rainer Simon <aboutgeo@gmail.com>
2022-05-31 12:19:02 +0200

    Handling cases where widget is initialized with null config

--- src/Config.js
+++ src/Config.js
@@ -1,15 +1,15 @@
 export const createConfig = config => ({
   // Widget height
-  height: config.height || 200,
+  height: config?.height || 200,
 
   // Tile URL
-  tileUrl: config.tileUrl || 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  tileUrl: config?.tileUrl || 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 
   // Default map marker origin
-  defaultOrigin: config.defaultOrigin || [0, 0],
+  defaultOrigin: config?.defaultOrigin || [0, 0],
 
   // Default map zoom
-  defaultZoom: config.defaultZoom || 7,
+  defaultZoom: config?.defaultZoom || 7,
 
-  search: config.search
+  search: config?.search
 });
